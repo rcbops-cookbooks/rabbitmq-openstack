373501555df0053195131c53ebd9bad1cb3155ba
galstrom21 <jshepher@rackspace.com>
2012-08-03 16:50:29 -0500

    Adding monitoring configuration for rabbitmq-server

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,16 @@
-default['rabbitmq']['services']['']['scheme'] = "tcp"
-default['rabbitmq']['services']['']['port'] = "5672"
-default['rabbitmq']['services']['']['network'] = "nova"
+default['rabbitmq']['services']['queue']['scheme'] = "tcp"
+default['rabbitmq']['services']['queue']['port'] = "5672"
+default['rabbitmq']['services']['queue']['network'] = "nova"
+
+case platform
+when "fedora", "redhat"
+  default["rabbitmq"]["platform"] = {
+    "rabbitmq_service" = > "rabbitmq-server",
+    "rabbitmq_service_regex" => "-config /etc/rabbitmq/rabbitmq"
+  }
+when "ubuntu"
+  default["rabbitmq"]["platform"] = {
+    "rabbitmq_service" = > "rabbitmq-server",
+    "rabbitmq_service_regex" => "-config /etc/rabbitmq/rabbitmq"
+  }
+end
--- recipes/server.rb
+++ recipes/server.rb
@@ -1,1 +1,30 @@
+
+include_recipe "osops-utils"
+platform_options = node["rabbitmq"]["platform"]
+
+# Lookup endpoint info, and properly set rabbit attributes
+rabbit_info = get_bind_endpoint("rabbitmq", "queue")
+node.set["rabbitmq"]["port"] = rabbit_info["port"]
+node.set["rabbitmq"]["address"] = rabbit_info["host"]
+
+# TODO(shep): Using the 'guest' user because it gets special permissions
+#             we should probably setup different users for nova and glance
+# TODO(shep): Should probably use Opscode::OpenSSL::Password for default_password
+
 include_recipe "rabbitmq::default"
+
+monitoring_procmon "rabbitmq-server" do
+  service_name=platform_options["rabbitmq_service"]
+
+  process_name platform_options["rabbitmq_service_regex"]
+  start_cmd "/usr/sbin/service #{service_name} start"
+  stop_cmd "/usr/sbin/service #{service_name} stop"
+end
+
+monitoring_metric "rabbitmq-server-proc" do
+  type "proc"
+  proc_name "rabbitmq-server"
+  proc_regex platform_options["rabbitmq_service_regex"]
+
+  alarms(:failure_min => 1.0)
+end
