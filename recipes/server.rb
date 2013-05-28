c8711d0a6af94340b7adc9929997ceda7a0e1d4a
Andy McCrae <andy.mccrae@googlemail.com>
2013-05-28 09:28:34 +0100

    Allow ability to override vrid

--- recipes/server.rb
+++ recipes/server.rb
@@ -107,7 +107,12 @@ if rcb_safe_deref(node, "vips.rabbitmq-queue")
   # TODO(anyone): fix this in a way that lets us run multiple clusters in the
   #               same broadcast domain.
   # this doesn't solve for the last octect == 255
-  router_id = vip.split(".")[3].to_i + 1
+  if node["rabbitmq"]["ha"]["vrid"] > 0 and node["rabbitmq"]["ha"]["vrid"] < 256
+    router_id = node["rabbitmq"]["ha"]["vrid"]
+  else
+    Chef::Log.info("Invalid or no vrid set - defaulting to last octet of IP + 1")
+    router_id = vip.split(".")[3].to_i + 1
+  end
 
   keepalived_chkscript "rabbitmq" do
     script "#{platform_options["service_bin"]} rabbitmq-server status"
